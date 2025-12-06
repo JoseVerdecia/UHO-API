983a4625305d681b28dde572e5d21c79f08dcb5a
JoseVerdecia <joseosvaldoverdeciaargota@gmail.com>
2025-12-06 08:34:29 -0500

    gitignore x4

--- UHO-API/obj/Debug/net10.0/UHO-API.AssemblyInfo.cs
+++ UHO-API/obj/Debug/net10.0/UHO-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UHO-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0375e8ac2dfb10abbb4a5c185b4b485da0dff64a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe7a3e3e36fd3d3c1e56d296b7e36189915a2407")]
 [assembly: System.Reflection.AssemblyProductAttribute("UHO-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UHO-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
